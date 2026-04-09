530c77cf13432cf6bf0df9b750d897d95ac88b1d
ZeroTrinta <152907776+ZeroTrinta@users.noreply.github.com>
2026-04-09 18:31:47 -0300

    Delete omniscriptum directory

--- omniscriptum/js/init.js
+++
@@ -1,9 +0,0 @@
-/* ===== INIT ===== */
-window.addEventListener('resize', resize);
-resize();
-buildFilters();
-requestAnimationFrame(draw);
-initAudio();
-waitForFB(() => {
-  runInitialLoading().then(() => initAuth());
-});
